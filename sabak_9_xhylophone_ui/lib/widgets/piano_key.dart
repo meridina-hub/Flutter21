68efb39b81fdc97d1d36f9254720a25725eccd0f
meridina <keldibekovameridina@gmail.com>
2025-09-28 20:07:51 +0600

    butpoi kaldy

--- sabak_9_xhylophone_ui/lib/widgets/piano_key.dart
+++ sabak_9_xhylophone_ui/lib/widgets/piano_key.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
 class PianoKey extends StatelessWidget {
-  const PianoKey({super.key, required this.colors});
+  const PianoKey({super.key, required this.colors, this.onTap});
   final Color colors;
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 91,
-      decoration: BoxDecoration(color: colors),
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        height: 91,
+        decoration: BoxDecoration(color: colors),
+      ),
     );
   }
 }
